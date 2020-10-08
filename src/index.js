3a14993410c80bf7189a6a81d1412f948adc4c32
elleon003 <ngrandison@ymail.com>
2020-10-08 13:19:10 -0400

    checkpoint 10

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-// import HelloWorld from './state-drills/HelloWorld';
-// import Bomb from './state-drills/Bomb';
-import RouletteGun from './state-drills/RouletteGun';
+import App from './App';
 
-ReactDOM.render(<RouletteGun />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
